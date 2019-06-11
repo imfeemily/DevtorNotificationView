18206d6854983a77e852daaa64fc02d47b3c4ae2
Supakorn Thitivarangkool <ifeemily@gmail.com>
2019-06-11 18:03:01 +0700

    initial commit

---
+++ DevtorNotification/DevtorNotificationView.swift
@@ -0,0 +1,35 @@
+//
+//  DevtorNotificationView.swift
+//  DevtorNotification
+//
+//  Created by Supakorn Thitivarangkool on 11/6/2562 BE.
+//  Copyright © 2562 Feem.io. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DevtorNotificationView: UIView {
+    
+    let colors : [UIColor] = [.red, .orange, .yellow, .green, .blue, .purple]
+    var colorCounter = 0
+    /*
+     This method will be called when a frame is passed to the constructor of DevtorNotificationView.
+     Will customize properties here.
+    */
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        let scheduledColorChanged = Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true) { (timer) in
+            UIView.animate(withDuration: 2.0) {
+                self.layer.backgroundColor = self.colors[self.colorCounter % 6].cgColor
+                self.colorCounter+=1
+            }
+        }
+        scheduledColorChanged.fire()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
---
+++ DevtorNotification/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DevtorNotification
+//
+//  Created by Supakorn Thitivarangkool on 11/6/2562 BE.
+//  Copyright © 2562 Feem.io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let devtorView = DevtorNotificationView(frame: self.view.bounds)
+        self.view.addSubview(devtorView)
+    }
+
+
+}
+
